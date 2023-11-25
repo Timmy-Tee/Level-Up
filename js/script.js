985dd1ba9e7f44ac2e21a09d47a2cba56d076269
Egbetokun Timilehin <timmyaof02@gmail.com>
2023-11-25 10:15:20 -1200

    Worked on the notification and users details popup

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 let cross = document.getElementById("cross")
 let plan = document.getElementById("plan")
 let details = document.querySelectorAll("#prod1")
+// User Info drop down
 let usdp = document.getElementById("user_drop");
 let userDetails = document.getElementById("user_details")
+
+// Bell
+let userBellIcon = document.getElementById('bell')
+let usersNotification = document.getElementById('notification')
+
+let a = 0;   
+
+let flex = "flex";
+let none = "none";
+
 
 
 cross.addEventListener('click', ()=>{
@@ -20,24 +31,29 @@ details.forEach((detailTag)=>{
 })
 
 
-userDetails.addEventListener("click", ()=>{
-    usdp.style.display = "flex";
-
-    if(usdp.style.display == "flex"){
-        userDetails.addEventListener('click', ()=>{
-            usdp.style.display = "none";
-             
-            if(usdp.style.display == "none"){
-                userDetails.addEventListener('click', ()=>{
-                usdp.style.display = "flex";
-                })
-            }
-            else{
-                usdp.style.display = "none";
-            }
-        })
+userDetails.addEventListener('click', ()=>{
+    a += 1
+    if((a % 2) == 1){
+        usdp.style.display = flex;
     }
-     
-    
+    else if((a % 2) == 0){
+        usdp.style.display = none;
+    }
 })
+
+console.log(a)
+
+userBellIcon.addEventListener('click', ()=>{
+    a += 1 
+    if((a % 2) == 1){
+        usersNotification.style.display = flex;
+    }
+    else if((a % 2) == 0){
+        usersNotification.style.display = none;
+    }
+})
+
+
+
+
 
