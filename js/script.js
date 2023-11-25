addd0b4acc8a6db116709747fd59263f2bac15ad
Egbetokun Timilehin <timmyaof02@gmail.com>
2023-11-24 19:27:52 -1200

    added the notification and users dropdown

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let cross = document.getElementById("cross")
 let plan = document.getElementById("plan")
 let details = document.querySelectorAll("#prod1")
+let usdp = document.getElementById("user_drop");
+let userDetails = document.getElementById("user_details")
+
 
 cross.addEventListener('click', ()=>{
     plan.style.display="none"
@@ -15,3 +18,26 @@ details.forEach((detailTag)=>{
         })
     })
 })
+
+
+userDetails.addEventListener("click", ()=>{
+    usdp.style.display = "flex";
+
+    if(usdp.style.display == "flex"){
+        userDetails.addEventListener('click', ()=>{
+            usdp.style.display = "none";
+             
+            if(usdp.style.display == "none"){
+                userDetails.addEventListener('click', ()=>{
+                usdp.style.display = "flex";
+                })
+            }
+            else{
+                usdp.style.display = "none";
+            }
+        })
+    }
+     
+    
+})
+
